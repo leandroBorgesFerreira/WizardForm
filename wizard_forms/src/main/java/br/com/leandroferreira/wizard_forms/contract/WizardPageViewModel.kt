3ba799a4a831349497fe37c1894a842d0e94ecb1
Leandro Ferreira <lehen01@gmail.com>
2017-08-28 15:03:08 -0300

    0.2.2 version

--- wizard_forms/src/main/java/br/com/leandroferreira/wizard_forms/contract/WizardPageViewModel.kt
+++ wizard_forms/src/main/java/br/com/leandroferreira/wizard_forms/contract/WizardPageViewModel.kt
@@ -1,6 +1,8 @@
 package br.com.leandroferreira.wizard_forms.contract
 
-abstract class WizardPageViewModel<K> {
+import android.databinding.BaseObservable
+
+abstract class WizardPageViewModel<K> : BaseObservable() {
     var navigator : Navigator? = null
     var stateHolder : StateHolder<K>? = null
 }
