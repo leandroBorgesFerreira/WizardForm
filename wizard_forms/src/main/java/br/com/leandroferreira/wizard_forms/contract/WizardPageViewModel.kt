2a3846c62176a7c18ba17255f527191c4fc4f0cd
Leandro Ferreira <lehen01@gmail.com>
2017-08-28 18:44:09 -0300

    moving dependencies to constructor

--- wizard_forms/src/main/java/br/com/leandroferreira/wizard_forms/contract/WizardPageViewModel.kt
+++ wizard_forms/src/main/java/br/com/leandroferreira/wizard_forms/contract/WizardPageViewModel.kt
@@ -2,7 +2,5 @@ package br.com.leandroferreira.wizard_forms.contract
 
 import android.databinding.BaseObservable
 
-abstract class WizardPageViewModel<K> : BaseObservable() {
-    var navigator: WizardNavigator? = null
-    var stateHolder: WizardStateHolder<K>? = null
-}
+abstract class WizardPageViewModel<K>(var navigator: WizardNavigator? = null,
+                                      var stateHolder: WizardStateHolder<K>? = null) : BaseObservable()
